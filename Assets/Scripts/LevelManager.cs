619e2783e9b0e4afbfde716ee9c9942302ffb64a
Pedro Marchiori <pmarchiori08@gmail.com>
2024-04-04 17:58:10 -0300

    Placing tiles

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private GameObject tile;
     void Start()
+    {
+        CreateLevel();
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CreateLevel()
     {
-        
+        float tileSize = tile.GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+        for (int y = 0; y < 5; y++)
+        {
+            for (int x = 0; x < 5; x++)
+            {
+                GameObject newTile = Instantiate(tile);
+                newTile.transform.position = new Vector3(tileSize * x, tileSize * y, 0);
+            }
+        }
     }
 }
