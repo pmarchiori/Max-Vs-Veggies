2ddc0d928c93b84c1acec2eceee79ef7f0cadb39
Pedro Marchiori <pmarchiori08@gmail.com>
2024-04-05 15:25:15 -0300

    tiles from text file

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,13 +18,10 @@ public class LevelManager : MonoBehaviour
 
     private void CreateLevel() //creates our level
     {
-        string[] mapData = new string[]
-        {
-            "111", "000", "101", "001", "110"
-        };
+        string[] mapData = ReadLevelText();
 
-        int mapX = mapData[0].ToCharArray().Length;
-        int mapY = mapData.Length;
+        int mapX = mapData[0].ToCharArray().Length; //calculates the x map size
+        int mapY = mapData.Length; //calculates the y map size
 
         Vector3 worldStart = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height)); //calculate the world start point (top left corner)
         for (int y = 0; y < mapY; y++)
@@ -39,6 +37,7 @@ public class LevelManager : MonoBehaviour
 
     private void PlaceTile(string tileType, int x, int y, Vector3 worldStart)
     {
+        //parses tileType into an int to use if as an indexer when creating a new tile
         int tileIndex = int.Parse(tileType);
 
         //creates a new tile and makes a reference for that tile in the newTile variable
@@ -46,5 +45,14 @@ public class LevelManager : MonoBehaviour
 
         //uses newTile to change the position of the tile
         newTile.transform.position = new Vector3(worldStart.x + (TileSize * x), worldStart.y - (TileSize * y), 0);
+    }
+
+    private string[] ReadLevelText()
+    {
+        TextAsset bindData = Resources.Load("Level") as TextAsset;
+
+        string data = bindData.text.Replace(Environment.NewLine, string.Empty);
+
+        return data.Split('-');
     }
 }
