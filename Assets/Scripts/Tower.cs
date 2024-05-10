cd85408dca170261229c5c73b22c97b11f05d6d6
Pedro Marchiori <pmarchiori08@gmail.com>
2024-05-10 15:30:20 -0300

    Tower Enemy Detection

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -6,6 +6,10 @@ public class Tower : MonoBehaviour
 {
     private SpriteRenderer spriteRenderer;
 
+    private Enemy target;
+
+    private Queue<Enemy> enemies = new Queue<Enemy>();
+
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
@@ -14,11 +18,37 @@ public class Tower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Attack();
+        Debug.Log(target);
     }
 
     public void Select()
     {
         spriteRenderer.enabled = !spriteRenderer.enabled;
     }
+
+    private void Attack()
+    {
+        if(target == null && enemies.Count > 0)
+        {
+            target = enemies.Dequeue();
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Enemy")
+        {
+            enemies.Enqueue(other.GetComponent<Enemy>());
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.tag == "Enemy")
+        {
+            target = null;
+        }
+    }
+
 }
